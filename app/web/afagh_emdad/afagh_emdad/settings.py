637ceea9a33fc9e24c635289c7428441790ff789
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-06-01 01:50:40 +0330

    Define TIME_ZONE

--- app/web/afagh_emdad/afagh_emdad/settings.py
+++ app/web/afagh_emdad/afagh_emdad/settings.py
@@ -169,6 +169,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
+LANGUAGE_CODE = "en-us"
+# تنظیمات زمان ایران
+TIME_ZONE = "Asia/Tehran"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
